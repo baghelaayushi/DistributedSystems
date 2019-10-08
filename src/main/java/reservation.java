796b17786d67f1efb0577fdb480cc24152fc82c7
Satej Sawant <satejs93@gmail.com>
2019-10-07 22:34:28 -0400

    Cancel feature

--- src/main/java/reservation.java
+++ src/main/java/reservation.java
@@ -1,42 +1,66 @@
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class reservation {
     // status is the dictionary
-    private static HashMap<String,String> status = new HashMap<>();
+    private static HashMap<String,ClientInfo> status = new HashMap<>();
     private static HashMap<Integer,Integer> flights = new HashMap<>(20);
 
     // To check whether a client can reserve a flight or not
-    public String reserve(String clientId, String[] flight){
+    public String reserve(String reservation){
+
+        String input[] = reservation.split(" ");
+        String clientName = input[1];
+        String flightNumbers[] = input[2].split(",");
+
+
         // No client can reserve two flights
-        if(status!= null && status.containsKey(clientId))
+        if(status!= null && status.containsKey(clientName))
             return "You can't book more than one flights";
 
         // for all flights that the client wants
-        for(String flightNo: flight){
+        for(String flightNo: flightNumbers){
 
             // only allow flight booking if seats are available
             // else change request to pending
             if(flights.get(Integer.parseInt(flightNo))>=1) {
                 int seats = flights.get(Integer.parseInt(flightNo));
                 flights.replace(Integer.parseInt(flightNo),seats-1);
-                status.put(clientId, "pending");
             }
             else
                 return "Failed";
         }
+
+
+        status.put(clientName, new ClientInfo(clientName, flightNumbers, "pending"));
         return "The status is pending";
 
     }
-    public String reserveSeat(String reservation){
 
-        String input[] = reservation.split(" ");
+    public String cancel(String command){
+
+        String input[] = command.split(" ");
         String clientName = input[1];
-        String flightNumbers[] = input[2].split(",");
 
-        return reserve(clientName,flightNumbers);
+        if(status.containsKey(clientName)){
 
+
+            List<Integer> flightsToCancel = status.get(clientName).getFlights();
+
+            for (int i = 0; i < flightsToCancel.size(); i++){
+                int currentSeats = flights.get(flightsToCancel.get(i));
+                currentSeats++;
+                flights.put(flightsToCancel.get(i), currentSeats);
+            }
+
+            status.remove(clientName);
+
+        }else{
+            return "Cannot schedule reservation for "+ clientName;
+        }
+
+        return "Reservation for "+ clientName + "cancelled.";
     }
+
 
     public static void main( String[] args) {
 
@@ -56,12 +80,16 @@ public class reservation {
 
             String input[] = userInput.split(" ");
             String command = input[0];
+            String response;
 
             switch (command) {
                 case "reserve":
-                    ob.reserveSeat(userInput);
+                    response = ob.reserve(userInput);
+                    System.out.println(response);
                     break;
                 case "cancel":
+                    response = ob.cancel(userInput);
+                    System.out.println(response);
                     break;
 
                 default:
@@ -71,5 +99,29 @@ public class reservation {
         }
 
     }
+}
+
+class ClientInfo{
+
+    String clientName;
+    String status;
+    List<Integer> flightNumbers = new ArrayList<>();
+
+
+    public ClientInfo(String clientName, String[] flightNumbers, String status){
+        this.clientName = clientName;
+        this.status = status;
+        for (String flight :  flightNumbers){
+            this.flightNumbers.add(Integer.parseInt(flight));
+        }
+
+
+    }
+
+    public List<Integer> getFlights(){
+        return flightNumbers;
+    }
+
+
 }
 
