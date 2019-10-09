522532fc19a5d61aad285fe56674be2d0390ef35
aayushi.baghel <43557845+aayushi123baghel@users.noreply.github.com>
2019-10-08 21:51:21 -0400

    Matrix/Log Update

---
+++ src/main/java/Event.java
@@ -0,0 +1,15 @@
+import java.sql.Timestamp;
+
+public class Event {
+    private String operationType;
+    private ClientInfo operation;
+    private Timestamp time;
+    private int NodeId;
+
+    public Event(String operationType,ClientInfo operation,int NodeId){
+        this.operationType = operationType;
+        this.operation = operation;
+        this.NodeId = NodeId;
+        this.time = new Timestamp(System.currentTimeMillis());
+    }
+}
