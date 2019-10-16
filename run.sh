69dcdd29cfbff08f33764f1fb42ab0c0c26b378f
Satej Sawant <satejs93@gmail.com>
2019-10-16 16:57:18 -0400

    Attempt to fix classpath issues 10

--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Script executed from: ${PWD}"
-
-cd bin
-
-for entry in "$search_dir"/*
-do
-  echo "$entry"
-done
-
-
 # Run my program.
 java -cp ./dependencies/*:. Server $1
 
