efcbe4e003b081d3950f91d2b2b06c1de98f7ec8
Satej Sawant <satejs93@gmail.com>
2019-10-16 16:26:04 -0400

    Attempt to fix classpath issues 5

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+echo "Script executed from: ${PWD}"
+
+BASEDIR=$(dirname $0)
+echo "Script location: ${BASEDIR}"
+
+
+
 # Run my program.
-java -cp .:/dependencies/* Server $1
+java -cp ./dependencies/*:. Server $1
 
 exit 0
