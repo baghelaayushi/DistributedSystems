d6669d4321357cb621a356e92cc06fbb945649d6
Satej Sawant <satejs93@gmail.com>
2019-10-16 16:30:31 -0400

    Attempt to fix classpath issues 6

--- run.sh
+++ run.sh
@@ -2,12 +2,13 @@
 
 echo "Script executed from: ${PWD}"
 
-BASEDIR=$(dirname $0)
-echo "Script location: ${BASEDIR}"
-
+for entry in "$search_dir"/*
+do
+  echo "$entry"
+done
 
 
 # Run my program.
-java -cp ./dependencies/*:. Server $1
+java -cp ./dependencies/*:./src/main/java Server $1
 
 exit 0
