3cb554e5d15310d008d7be1007beb4a542c05485
Satej Sawant <satejs93@gmail.com>
2019-10-16 18:39:47 -0400

    update to run file

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Run my program.
-java -cp "./bin/commons-lang3-3.5.jar:./bin/gson-2.8.6.jar:" Server $1
+java -cp ./commons-lang3-3.5.jar:./gson-2.8.6.jar: Server $1
 
 exit 0
