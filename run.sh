fecf836ec474a0cc92fe3f201da493949877fe38
Satej Sawant <satejs93@gmail.com>
2019-10-16 18:36:48 -0400

    update to run file

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 # Run my program.
-java -cp gson-2.8.6.jar: Server $1
-
-java -cp "/bin/commons-lang3-3.5.jar:/bin/gson-2.8.6.jar:" qrcode
+java -cp "./bin/commons-lang3-3.5.jar:./bin/gson-2.8.6.jar:" Server $1
 
 exit 0
